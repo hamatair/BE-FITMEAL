c1878c43bc5869523a610b557467265f62b49445
ilham <akabeaer212@gmail.com>
2024-03-18 12:51:16 +0630

    style : hapus gorm

--- model/user.go
+++ model/user.go
@@ -7,20 +7,20 @@ type Register struct {
 	UserName    string    `json:"userName" binding:"required"`
 	Email       string    `json:"email" binding:"required,email"`
 	Password    string    `json:"password" binding:"required"`
-	Aktivitas   string    `json:"aktivitas" gorm:"type:varchar(255);not null;" binding:"required"`
-	Gender      string    `json:"gender" gorm:"type:varchar(255);not null;" binding:"required"`
-	Umur        uint      `json:"umur" gorm:"type:varchar(255);not null;" binding:"required"`
-	Alamat      string    `json:"alamat" gorm:"type:varchar(255);not null;" binding:"required"`
+	Aktivitas   string    `json:"aktivitas" binding:"required"`
+	Gender      string    `json:"gender" binding:"required"`
+	Umur        uint      `json:"umur" binding:"required,number"`
+	Alamat      string    `json:"alamat" binding:"required"`
 	BeratBadan  uint      `json:"beratBadan" binding:"required,number"`
 	TinggiBadan uint      `json:"tinggiBadan" binding:"required,number"`
 }
 
 type EditProfile struct {
-	UserName    string `json:"userName" binding:"required"`
-	Umur        uint   `json:"umur" gorm:"not null;" binding:"required"`
-	Alamat      string `json:"alamat" gorm:"type:varchar(255);not null;" binding:"required"`
-	BeratBadan  uint   `json:"beratBadan" gorm:"not null;" binding:"required,number"`
-	TinggiBadan uint   `json:"tinggiBadan" gorm:"not null;" binding:"required,number"`
+	UserName    string `json:"userName"`
+	Umur        uint   `json:"umur"`
+	Alamat      string `json:"alamat"`
+	BeratBadan  uint   `json:"beratBadan"`
+	TinggiBadan uint   `json:"tinggiBadan"`
 }
 
 type ChangePassword struct {
