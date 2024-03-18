5beb9476fe148816260e6e700b40daad331f4e1f
ilham <akabeaer212@gmail.com>
2024-03-18 13:02:18 +0630

    feat : menampilkan informasi nutrisi pada data user

--- internal/service/user.go
+++ internal/service/user.go
@@ -13,8 +13,8 @@ import (
 
 type UserServiceInterface interface {
 	FindAll() ([]entity.User, error)
-	Create(user model.Register) (entity.User, error)
-	UserEditProfile(user model.EditProfile, id string) (entity.User, error)
+	Create(nuser model.Register) (entity.User, error)
+	UserEditProfile(nuser model.EditProfile, id string) (entity.User, error)
 	Login(param model.Login) (model.LoginResponse, error)
 	GetUser(param model.UserParam) (entity.User, error)
 	UserChangePassword(param model.ChangePassword, id string) (entity.User, error)
@@ -34,25 +34,78 @@ func NewUserService(repository repository.UserRepositoryInterface, bcrypt bcrypt
 	}
 }
 
+// func hitungNutrisi(nuser entity.User) entity.User {
+// 	var kalori float32
+// 	if nuser.Gender == "male" {
+// 		kalori = 66 + (13.7*float32(nuser.BeratBadan) + (5 * float32(nuser.TinggiBadan)) - (6.8 * float32(nuser.Umur)))
+// 	} else if nuser.Gender == "female" {
+// 		kalori = 655 + (9.6*float32(nuser.BeratBadan) + (1.8 * float32(nuser.TinggiBadan)) - (4.7 * float32(nuser.Umur)))
+// 	}
+
+// 	if nuser.Aktivitas == "sangat jarang olahraga" {
+// 		kalori *= 1.2
+// 	} else if nuser.Aktivitas == "jarang olahraga" {
+// 		kalori *= 1.375
+// 	} else if nuser.Aktivitas == "sering olahraga" {
+// 		kalori *= 1.55
+// 	} else if nuser.Aktivitas == "sangat sering olahraga" {
+// 		kalori *= 1.725
+// 	}
+
+// 	protein := (0.15 * kalori) / 4
+// 	karbohidrat := (0.6 * kalori) / 4
+// 	lemak := (0.15 * kalori) / 9
+
+// 	nuser.Kalori = kalori
+// 	nuser.Protein = protein
+// 	nuser.Karbohidrat = karbohidrat
+// 	nuser.Lemak = lemak
+
+// 	return nuser
+// }
+
 func (u *UserService) Create(param model.Register) (entity.User, error) {
 	hashPassword, err := u.bcrypt.GenerateFromPassword(param.Password)
 	if err != nil {
 		return entity.User{}, err
 	}
 
-	param.ID = uuid.New()
-	param.Password = hashPassword
+	var kalori float32
+	if param.Gender == "male" {
+		kalori = 66 + (13.7*float32(param.BeratBadan) + (5 * float32(param.TinggiBadan)) - (6.8 * float32(param.Umur)))
+	} else if param.Gender == "female" {
+		kalori = 655 + (9.6*float32(param.BeratBadan) + (1.8 * float32(param.TinggiBadan)) - (4.7 * float32(param.Umur)))
+	}
+
+	if param.Aktivitas == "sangat jarang olahraga" {
+		kalori *= 1.2
+	} else if param.Aktivitas == "jarang olahraga" {
+		kalori *= 1.375
+	} else if param.Aktivitas == "sering olahraga" {
+		kalori *= 1.55
+	} else if param.Aktivitas == "sangat sering olahraga" {
+		kalori *= 1.725
+	}
+
+	protein := (0.15 * kalori) / 4
+	karbohidrat := (0.6 * kalori) / 4
+	lemak := (0.15 * kalori) / 9
+
 	nuser := entity.User{
-		ID:          param.ID,
+		ID:          uuid.New(),
 		UserName:    param.UserName,
 		Email:       param.Email,
-		Password:    param.Password,
+		Password:    hashPassword,
 		Aktivitas:   param.Aktivitas,
 		Gender:      param.Gender,
 		Umur:        param.Umur,
 		Alamat:      param.Alamat,
 		BeratBadan:  param.BeratBadan,
 		TinggiBadan: param.TinggiBadan,
+		Kalori:      kalori,
+		Protein:     protein,
+		Karbohidrat: karbohidrat,
+		Lemak:       lemak,
 	}
 
 	newUser, err := u.userRepository.Create(nuser)
@@ -62,13 +115,13 @@ func (u *UserService) Create(param model.Register) (entity.User, error) {
 }
 
 func (u *UserService) FindAll() ([]entity.User, error) {
-	user, err := u.userRepository.FindAll()
+	nuser, err := u.userRepository.FindAll()
 
-	return user, err
+	return nuser, err
 }
 
-func (u *UserService) UserEditProfile(user model.EditProfile, id string) (entity.User, error) {
-	UserPersonalization, err := u.userRepository.UserEditProfile(user, id)
+func (u *UserService) UserEditProfile(nuser model.EditProfile, id string) (entity.User, error) {
+	UserPersonalization, err := u.userRepository.UserEditProfile(nuser, id)
 	if err != nil {
 		fmt.Println("service", err)
 	}
@@ -79,25 +132,25 @@ func (u *UserService) UserEditProfile(user model.EditProfile, id string) (entity
 func (u *UserService) Login(param model.Login) (model.LoginResponse, error) {
 	result := model.LoginResponse{}
 
-	user, err := u.userRepository.GetUser(model.UserParam{
+	nuser, err := u.userRepository.GetUser(model.UserParam{
 		Email: param.Email,
 	})
 	if err != nil {
 		return result, err
 	}
 
-	err = u.bcrypt.CompareAndHashPassword(user.Password, param.Password)
+	err = u.bcrypt.CompareAndHashPassword(nuser.Password, param.Password)
 	if err != nil {
 		return result, err
 	}
 
-	token, err := u.jwtAuth.CreateJWTToken(user.ID)
+	token, err := u.jwtAuth.CreateJWTToken(nuser.ID)
 	if err != nil {
 		return result, err
 	}
 
 	result.Token = token
-	result.ID = user.ID
+	result.ID = nuser.ID
 
 	return result, nil
 }
@@ -127,10 +180,10 @@ func (u *UserService) UserChangePassword(param model.ChangePassword, id string) 
 	}
 
 	param.NewPassword = newpassword
-	user, err := u.userRepository.UserChangePassword(param, id)
+	nuser, err := u.userRepository.UserChangePassword(param, id)
 	if err != nil {
 		fmt.Println("service", err)
 	}
 
-	return user, err
+	return nuser, err
 }
