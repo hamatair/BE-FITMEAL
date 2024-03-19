a2e40cb0332bdccae75dd62731b5cda80ff9235f
ilham <akabeaer212@gmail.com>
2024-03-19 22:15:02 +0630

    docs : login hanya mengembalikan token dan tanpa param

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -37,14 +37,17 @@ func (h *Handler) EndPoint() {
 	v1 := h.Router.Group("/v1")
 
 	v1.GET("/user/get", h.GetAllDataUser)
-	v1.GET("/user/get/:id", h.GetUserById)
+	v1.GET("/user/get-user-profile",h.Middleware.AuthenticateUser, h.GetUserById)
 	v1.POST("/meal", h.NewDataMeal)
 	v1.GET("/meal/get", h.GetAllDataMeal)
 	v1.POST("/user/register", h.UserRegisterAndPersonalization)
-	v1.PATCH("/user/edit-profile/:id",h.Middleware.AuthenticateUser, h.UserEditProfile)
-	v1.PATCH("/user/edit-profile/change-password/:id",h.Middleware.AuthenticateUser, h.changePasswordUser)
+	v1.PATCH("/user/edit-profile",h.Middleware.AuthenticateUser, h.UserEditProfile)
+	v1.PATCH("/user/edit-profile/change-password",h.Middleware.AuthenticateUser, h.changePasswordUser)
 	v1.POST("user/login", h.Login)
 	v1.POST("user/login-user", h.Middleware.AuthenticateUser, h.getLoginUser)
+	v1.POST("/user/forgot-password/get", h.CreateCodeVerification)
+	v1.POST("user/forgot-password", h.ForgotPasswordUser)
+	v1.POST("/user/forgot-password/change-password", h.ChangePasswordBeforeLogin)
 
 	port := os.Getenv("PORT")
 	if port == "" {
