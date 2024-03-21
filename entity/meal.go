60f2dffd9370c1f8e68b3cbcb6b727b8e73632e9
ilham <akabeaer212@gmail.com>
2024-03-22 02:03:15 +0630

    docs : float nutrition

--- entity/meal.go
+++ entity/meal.go
@@ -10,10 +10,10 @@ type Meal struct {
 	ID          uuid.UUID `json:"id" gorm:"primary_key;"`
 	Name        string    `json:"name" binding:"required"`
 	Jenis       string    `json:"jenis" binding:"required"`
-	Kalori      uint      `json:"kalori" binding:"required"`
-	Protein     uint      `json:"protein" binding:"required"`
-	Karbohidrat uint      `json:"karbohidrat" binding:"required"`
-	Lemak       uint      `json:"lemak" binding:"required"`
+	Kalori      float32      `json:"kalori" binding:"required"`
+	Protein     float32      `json:"protein" binding:"required"`
+	Karbohidrat float32      `json:"karbohidrat" binding:"required"`
+	Lemak       float32      `json:"lemak" binding:"required"`
 	CreatedAt   time.Time `json:"createdAt" gorm:"autoCreateTime"`
 	UpdatedAt   time.Time `json:"updatedAt" gorm:"autoUpdateTime"`
 }
--- model/meal.go
+++ model/meal.go
@@ -6,10 +6,10 @@ type NewMeal struct {
 	ID          uuid.UUID `json:"-"`
 	Name        string    `json:"name"`
 	Jenis       string    `json:"jenis"`
-	Kalori      uint      `json:"kalori"`
-	Protein     uint      `json:"protein"`
-	Karbohidrat uint      `json:"karbohidrat"`
-	Lemak       uint      `json:"lemak"`
+	Kalori      float32   `json:"kalori"`
+	Protein     float32   `json:"protein"`
+	Karbohidrat float32   `json:"karbohidrat"`
+	Lemak       float32   `json:"lemak"`
 }
 
 type cariMeal struct {
